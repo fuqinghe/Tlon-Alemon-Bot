420d1aab04ec01f44d6e7804e68bf03c06d820f5
Tloml-Starry <109225278+fuqinghe@users.noreply.github.com>
2023-05-29 13:20:58 +0800

    ```TS```

---
+++ 刷时长.ts
@@ -0,0 +1,70 @@
+import { plugin, Messagetype } from 'alemon'
+import axios from 'axios';
+import fs from 'fs'
+const dirpath = "example/刷时长"
+var filename = `刷.json`
+if (!fs.existsSync(dirpath)) {//如果文件夹不存在
+	fs.mkdirSync(dirpath);//创建文件夹
+}
+if (!fs.existsSync(dirpath + "/" + filename)) {//文件不存在
+    fs.writeFileSync(dirpath + "/" + filename, JSON.stringify({//创建文件
+    }))
+}
+
+export class 刷时长 extends plugin {
+    constructor() {
+        super({
+            rule: [
+                {
+                    reg: '^刷时长(.*)$',
+                    fnc: '刷时长'
+                },{
+                    reg: '^绑定QQ(.*)$',
+                    fnc: '绑定QQ'
+                }
+            ]
+        })
+    }
+    async 绑定QQ(e: Messagetype) {
+        var qq_id = e.msg.content.replace(/#|<@![0-9a-zA-Z]+> 绑定QQ/g, "")
+        var data = {
+            "qq": qq_id,
+        }
+        var id = e.msg.guild_id
+        var json = JSON.parse(fs.readFileSync(dirpath + "/" + filename, "utf8"));//读取文件
+        if(!json.hasOwnProperty(id)) {//如果json中不存在该用户
+            json[id] = data
+            fs.writeFileSync(dirpath + "/" + filename, JSON.stringify(json, null, "\t"));//写入文件
+            await e.reply("已记录您的QQ\n记录的QQ随时会失效,届时请重新绑定")
+        }
+        else{
+            json[id] = data
+            fs.writeFileSync(dirpath + "/" + filename, JSON.stringify(json, null, "\t"));//写入文件
+            await e.reply("已重新记录您的QQ")
+        }
+    }
+    async 刷时长(e: Messagetype) {
+        var json = JSON.parse(fs.readFileSync(dirpath + "/" + filename, "utf8"));//读取文件
+        var qq = e.msg.content.replace(/#|刷时长/g, "")
+        let 时间 = new Date
+        const 时 = `0${时间.getHours()}`.slice(-2);
+        const 分 = `0${时间.getMinutes()}`.slice(-2);
+        var id = e.msg.guild_id
+        if(json.hasOwnProperty(id)) {//如果json中存在该用户
+            await e.reply("使用已记录的QQ")
+            qq = JSON.stringify(json[id].qq)
+            qq = qq.slice(1, -1);
+        }
+        else{
+            await e.reply("还未绑定或已失效,请使用绑定QQ来让bot记住你")
+        }
+        const response = await axios.get(`http://q.dsbs.fun//api/tgsc?qq=${qq}`);
+        const res = response.data;
+        let num
+        if (res.code === 200 ){
+            num = `已为${qq}刷取QQ音乐\n听歌时长${时}时${分}分\n请前往QQ音乐查看排行榜`
+        }
+        e.reply(num);
+        return false
+    }
+}
